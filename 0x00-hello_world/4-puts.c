3a3d4cef6a2042e3acbbf94a7e3b96b31487d8c6
Abraham Dameh <dameh247@gmail.com>
2022-10-13 17:44:26 -0700

    commit c 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main: Entry point
+ * main - Entry point
  *
  * Description: 'the code to project c'
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
