bafce3b068567cad1948c9623c9c4b3ded2f9951
Abraham Dameh <dameh247@gmail.com>
2022-10-14 17:00:18 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main-entry point
+ *Return:0 if no error
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
