eb54cd81c3c6da11b7ca5c533707a55060ee504d
Abraham Dameh <dameh247@gmail.com>
2022-10-18 19:17:35 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _abs - print the abs of and int
+  * @t: the integer
+  * Return: the absolute value
+  */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
