306b0289da0c79b58800c152d9ab1ef92833e6bf
Abraham Dameh <dameh247@gmail.com>
2022-10-18 19:09:45 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_sign - prints the sign of a number
+  * @n: the character to print
+  * Return: 1 if greater than zero, 0 if = 0 and -1 if < 0
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
