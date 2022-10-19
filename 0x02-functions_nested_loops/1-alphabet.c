f399b9a79a84074a3d54079cd87d3d34cd212379
Abraham Dameh <dameh247@gmail.com>
2022-10-18 17:56:25 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - alphabets in lower case
+*/
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
