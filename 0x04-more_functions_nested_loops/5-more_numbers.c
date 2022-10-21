bebb9de2e187711d13862518643f49d01c57acea
Abraham Dameh <dameh247@gmail.com>
2022-10-20 17:31:36 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times numbers
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int num, a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
