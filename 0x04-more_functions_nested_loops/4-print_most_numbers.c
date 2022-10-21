74c71067b6278f308ff10375b9087564aa66e72d
Abraham Dameh <dameh247@gmail.com>
2022-10-20 17:11:53 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,8 +8,13 @@ void print_most_numbers(void)
 {
 	int a = 0;
 
-	while (a < 10 && a != 2 && a != 4)
-		_putchar(a++ + 0);
+	while (a < 10)
+	{
+		if (a != 2 and a != 4)
+			_putchar(a + '0');
 
-	_putchar('\n')
+		a++;
+	}
+
+	_putchar('\n');
 }
