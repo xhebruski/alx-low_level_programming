c1311027cc0b014c632b321a65f4d53b365c38df
Abraham Dameh <dameh247@gmail.com>
2022-10-20 17:05:11 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 1 to 9 excpt 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10 && a != 2 && a != 4)
+		_putchar(a++ + 0);
+
+	_putchar('\n')
+}
