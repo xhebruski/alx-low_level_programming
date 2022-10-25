91c9a3103f1169ac57e210f265e055f2529d02a2
Abraham Dameh <dameh247@gmail.com>
2022-10-24 18:09:02 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * puts_half - half of a string
+  * @str: string
+  * Return: print
+  */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
