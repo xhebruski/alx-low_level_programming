fb182dce09d2a8873f1b5c62903c24711ebf161c
Abraham Dameh <dameh247@gmail.com>
2022-11-03 17:48:42 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+void _puts_recursion(char *s);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int _putchar(char c);
+
+#endif
