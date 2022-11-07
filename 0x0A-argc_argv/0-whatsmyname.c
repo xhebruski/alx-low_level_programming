195171f5f4ab95eb8a55b27b8214ea4347be3bda
Abraham Dameh <dameh247@gmail.com>
2022-11-07 15:30:59 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - print the name of the progam
+  * @argc: argumnent count
+  * @argv: arguments
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Task for 0x0A-argc_argv
