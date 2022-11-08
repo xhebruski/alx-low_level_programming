5d14b7f1bcf65db0e3578ecb168877e60e286f11
Abraham Dameh <dameh247@gmail.com>
2022-11-07 16:27:51 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - print mul of 2 numbers
+  * @argc: counts
+  * @argv: arguments
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a *b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
