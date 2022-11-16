dfd6b42d396836ecff7db8cb8ca9e620508b676d
Abraham Dameh <dameh247@gmail.com>
2022-11-15 16:14:09 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - print the name of source code file
-*
-* Return: 0
-*/
-
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("%s\n", _FILE_);
